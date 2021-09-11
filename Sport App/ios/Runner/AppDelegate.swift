c6ebffbda3a27707d5765a2ff9c6b0f9c7997a9e
BerkBugur <56639375+BerkBugur@users.noreply.github.com>
2021-09-11 21:52:54 +0300

    V1
    v1

--- Sport App/ios/Runner/AppDelegate.swift
+++ Sport App/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKEY("AIzaSyARBBBto3y2tcBswrhzGhYHqDVvjqI6mfA")
+    GMSServices.provideAPIKEY("BURAYA API KEY GELCEK")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
